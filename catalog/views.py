5fe7fb2708311dabbbceebb591c797b5d295071c
joohyeong <joohyenog2001@gmail.com>
2020-01-03 12:02:37 +0900

    .

--- catalog/views.py
+++ catalog/views.py
@@ -1,41 +1,43 @@
+
 from django.shortcuts import render
 
 # Create your views here.
 
-from catalog.models import Book, Author, BookInstance, Genre
-from django.views import generic
+from .models import Book, Author, BookInstance, Genre
+
 
 def index(request):
     """View function for home page of site."""
-
     # Generate counts of some of the main objects
     num_books = Book.objects.all().count()
     num_instances = BookInstance.objects.all().count()
-     
-    # Available books (status = 'a')
+    # Available copies of books
     num_instances_available = BookInstance.objects.filter(status__exact='a').count()
-    
-    # The 'all()' is implied by default.    
-    num_authors = Author.objects.count()
-    
-    context = {
-        'num_books': num_books,
-        'num_instances': num_instances,
-        'num_instances_available': num_instances_available,
-        'num_authors': num_authors,
-    }
+    num_authors = Author.objects.count()  # The 'all()' is implied by default.
 
-    # Render the HTML template index.html with the data in the context variable
-    return render(request, 'index.html', context=context)
+    # Number of visits to this view, as counted in the session variable.
+    num_visits = request.session.get('num_visits', 0)
+    request.session['num_visits'] = num_visits+1
+
+    # Render the HTML template index.html with the data in the context variable.
+    return render(
+        request,
+        'index.html',
+        context={'num_books': num_books, 'num_instances': num_instances,
+                 'num_instances_available': num_instances_available, 'num_authors': num_authors,
+                 'num_visits': num_visits},
+    )
+
+
+from django.views import generic
+
 
 class BookListView(generic.ListView):
+    """Generic class-based view for a list of books."""
     model = Book
     paginate_by = 10
 
-from django.shortcuts import get_object_or_404
 
 class BookDetailView(generic.DetailView):
+    """Generic class-based detail view for a book."""
     model = Book
-def book_detail_view(request, primary_key):
-    book = get_object_or_404(Book, pk=primary_key)
-    return render(request, 'catalog/book_detail.html', context={'book': book})
