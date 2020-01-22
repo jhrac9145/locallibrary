0c8e4f654f4e469f91910a7b985732b0d00cd98e
JunhyunJunhyun <sji010900@gmail.com>
2020-01-22 20:15:27 +0900

    Part6 20200122.20.1

--- catalog/views.py
+++ catalog/views.py
@@ -8,7 +8,7 @@ def index(request):
     num_instances_available = BookInstance.objects.filter(status__exact='a').count() 
     num_authors = Author.objects.count()
     num_genre = Genre.objects.filter(name__iexact='nOvEL').count()
-    num_books_ = Book.objects.filter(title__iexact='maTilDa').count()
+    num_books_matilda = Book.objects.filter(title__iexact='maTilDa').count()
     
     context = {
         'num_books': num_books,
@@ -16,7 +16,7 @@ def index(request):
         'num_instances_available': num_instances_available,
         'num_authors': num_authors,
         'num_genre': num_genre,
-        'num_books_': num_books_,
+        'num_books_matilda': num_books_matilda,
     }
 
     return render(request, 'index.html', context=context)
