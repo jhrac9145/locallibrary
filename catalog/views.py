fad8074854e4fdeadee5c1d16373d20e7e8e37b8
joohyeong <joohyenog2001@gmail.com>
2020-01-22 17:57:29 +0900

    Part5 20200122 0

--- catalog/views.py
+++ catalog/views.py
@@ -8,13 +8,15 @@ def index(request):
     num_instances_available = BookInstance.objects.filter(status__exact='a').count() 
     num_authors = Author.objects.count()
     num_genre = Genre.objects.filter(name__iexact='nOvEL').count()
-
+    num_books_ = Book.objects.filter(title__iexact='maTilDa').count()
+    
     context = {
         'num_books': num_books,
         'num_instances': num_instances,
         'num_instances_available': num_instances_available,
         'num_authors': num_authors,
         'num_genre': num_genre,
+        'num_books_': num_books_,
     }
 
     return render(request, 'index.html', context=context)
