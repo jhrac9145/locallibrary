ffb2cef5a55b317bcd8375a098576a4d59438fec
JunhyunJunhyun <sji010900@gmail.com>
2020-01-23 11:59:47 +0900

    Part6 20200123.11.0

--- catalog/views.py
+++ catalog/views.py
@@ -7,21 +7,17 @@ from catalog.models import Book, Author, BookInstance, Genre
 def index(request):
     num_books = Book.objects.all().count()
     num_instances = BookInstance.objects.all().count()
-    num_instances_available = BookInstance.objects.filter(status__exact='a').count() 
+    num_instances_available = BookInstance.objects.filter(status__exact='a').count()
     num_authors = Author.objects.count()
-    num_genre = Genre.objects.filter(name__iexact='nOvEL').count()
-    num_books_matilda = Book.objects.filter(title__iexact='maTilDa').count()
     
     context = {
         'num_books': num_books,
         'num_instances': num_instances,
         'num_instances_available': num_instances_available,
         'num_authors': num_authors,
-        'num_genre': num_genre,
-        'num_books_matilda': num_books_matilda,
     }
 
-    return render(request, 'index.html', context=context)
+    return render(request, "index.html", context=context)
 
 
 class BookListView(generic.ListView):
